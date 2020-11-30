cacb58d776ee5921a8aafef5e024e4d914585554
simonproudfoot <simonproudfoot1980@gmail.com>
2020-11-30 13:19:13 +0000

    header titles

---
+++ components/title-bar.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="navbar shadow bg-white py-5 text-center">
+    <h1 class="text-2xl font-bold">
+        <template v-if="$store.state.currentSection == 1">
+            Client Vulnerability and Health Update
+        </template>
+        <template v-if="$store.state.currentSection == 2 && $store.state.formData.getConsent === 'No' || $store.state.formData.getConsent === null">
+            Vulnerability
+        </template>
+        <template v-if="$store.state.currentSection == 2 && $store.state.formData.getConsent === 'Yes'">
+            Vulnerability: Consents given
+        </template>
+    </h1>
+</div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,9 @@
 <template>
 <div>
-    <div class="navbar shadow relative block z-50 bg-white py-5 text-center w-full">
+    <!-- <div class="navbar shadow relative block z-50 bg-white py-5 text-center w-full">
         <h1 class="text-2xl font-bold">Client Vulnerability and Health Update</h1>
-    </div>
+    </div> -->
+    <title-bar></title-bar>
     <transition name="slide-down">
         <div class="bg-orange absolute z-40 w-full py-5 px-5 text-center font-bold text-white block h-auto" v-if="$store.state.currentSection !== 1">
             WARNING: Do not save changes or recalculate on Debtsolv during or after the use of this system. File must be closed without saving changes.
